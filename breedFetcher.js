354a9f142f38b5c3d622ecf052e807951ec95b57
github4maninder <maninder.email@gmail.com>
2021-11-04 19:34:00 -0400

    add file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,19 @@
 const request = require('request');
-const breed = process.argv[2];
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-request(url, (error, response, body) => {
-  const data = JSON.parse(body);
-  if (error){
-    console.log('Sorry, request failed - please provide correct information,', error);
-  } else {
-    if (data === undefined){
-      console.log('Sorry, breed is not found in our database.Look somewhere else!');
-    } else {
-      console.log(data[0].description);
+
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     }
-  }
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  console.log('data:', data[0].description);
-  // console.log('typeof data:', typeof data);
-  // console.log('typeof body:', typeof body);
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(null, data[0].description);
+    } else {
+      callback(`The breed ${breedName} not found in our database`, null);
+    }
+  });
+};
 
-});
+module.exports = { fetchBreedDescription };
